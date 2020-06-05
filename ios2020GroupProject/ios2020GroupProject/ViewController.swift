a46679d935bdd5f3a032026292db14e15800912f
ignaziogiambalvo <ignazio.giambalvo@gmail.com>
2020-06-05 13:54:06 +0200

    create project

---
+++ ios2020GroupProject/ios2020GroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios2020GroupProject
+//
+//  Created by Ignazio Giambalvo on 05/06/2020.
+//  Copyright © 2020 CodingAVision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
