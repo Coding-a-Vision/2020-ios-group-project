80d2a1d74061e4db3b704666b116f697615bf4ad
Samuele Rizzo <samuelerizzo97@outlook.com>
2020-06-12 12:06:56 +0200

    new swiftlint rules

--- ios2020GroupProject/ios2020GroupProject/AppDelegate.swift
+++ ios2020GroupProject/ios2020GroupProject/AppDelegate.swift
@@ -12,10 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    // swiftlint:disable line_length
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-    // swiftlint:enable line_length
+    
 }
